57cb3f7e8e35a4311929312ed997272995349d6b
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-04-13 00:30:09 -0400

    Update converter_fixm.js

--- converter_fixm.js
+++ converter_fixm.js
@@ -1,30 +1,26 @@
 var fs = require('fs');
 var util = require('util');
-var xml4js = require('./xml4js');
-var xml2js = require('xml2js');
+var xml4js = require('./xml4js'); 
 
 
 // Most of xml2js options should still work
 var options = {};
 var parser = new xml4js.Parser(options);
-
-
-// Default is not to download schemas automatically, so we should add it manually
-var xsd = fs.readFileSync('./tests/xsd/FIXM/Schema/applications/fficemessage/fficetemplates/flightdatarequest/FlightDataRequest.xsd', {encoding: 'utf-8'});
-var xml = fs.readFileSync('./tests/xml/FIXM/PROPOSAL_RESP.xml', {encoding: 'utf-8'});
-
 var parseString = require('xml2js').parseString;
 
-parser.addSchema('http://www.fixm.aero/app/ffice2/1.0', xsd, function (err, importsAndIncludes) { 
-    // importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in xsd file
-    
-    //convert xml to json
+// Adding it manually
+var xsd = fs.readFileSync('./tests/xsd/FIXM/FficeTRP_FficeMessage.xsd', {encoding: 'utf-8'});
+var xml = fs.readFileSync('./tests/xml/FIXM/E_TRIAL_RESP.xml', {encoding: 'utf-8'});
+
+
+parser.addSchema('http://www.fixm.aero/app/ffice/1.0', xsd, function (err, importsAndIncludes) { 
+    //importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in xsd file
+    //converting
     parseString(xml, function (err, result) {
-        //var json = JSON.stringify(result, null, 4);
-        console.log(util.inspect(result, false, null));
-        console.dir(result.Ffice2Message);
+        console.log(util.inspect(result, false, null)); //displays the whole result
+        console.dir(result);
         var json = JSON.stringify(result, null, 2);
 
-        fs.writeFileSync('./tests/output_fixm/AGREED_TRAJECTORY.json', json);
+        fs.writeFileSync('./tests/output_fixm/E_TRIAL_RESP.json', json);
     });
 });
